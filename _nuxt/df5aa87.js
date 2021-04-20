f2b57f2d888d129022b48912a21cc387772704f9
healvi <57001337+healvi@users.noreply.github.com>
2021-04-20 18:20:14 +0700

    Updates

---
+++ _nuxt/df5aa87.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{573:function(n,e,t){"use strict";t.r(e);var l={},c=t(4),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("h1",[n._v("class")])}),[],!1,null,null,null);e.default=component.exports}}]);
